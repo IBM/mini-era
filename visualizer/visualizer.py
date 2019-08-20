7f3466e1184e393aa3a072d3028b1a82e96d4ced
Alice Song <alice.song@Alices-MacBook-Pro.local>
2019-08-20 17:24:57 -0400

    more documentation

--- visualizer/visualizer.py
+++ visualizer/visualizer.py
@@ -51,7 +51,7 @@ MOVE_DOWN = 500 # every 500ms
 # HELPER FUNCTIONS
 def set_background():
     """
-    Initializes Visualizer's screen.
+    Objective: Initializes Visualizer's screen.
     Draws grass, road, and tree background.
     """
     # Clear screen
@@ -80,7 +80,7 @@ def get_img(path):
 
 def parse_trace(filename):  
     """
-    Parces trace files
+    Objective: Parces trace files
     Returns: 3 lists corresponding to 'columns' of trace file
         left_lane: list containing left lane's trace info for each epoch; ex) [011010111100, 011011101110, 011100100000, ...]
         mid_lane: list containing middle lane's trace info for each epoch
@@ -102,10 +102,22 @@ def parse_trace(filename):
     return left_lane, mid_lane, right_lane
 
 def get_object(bit_str):
+    """
+    Objective: Get the type of object in the lane
+    Returns: String of first 2 bits of the trace info; ex) '01' for car
+    Parameters:
+        bit_str: 12-bit lengthed trace
+    """
     obj = bit_str[0:2]
     return obj # return first 2 bits
 
 def get_dist(bit_str):
+    """
+    Objective: Calculate how far away an object is from the main car
+    Returns: Int of distance away in pixels
+    Parameters:
+        bit_str: 12-bit lengthed trace
+    """
     # Define distance scale
     dist_scale = 500 / 1023
     
@@ -115,6 +127,12 @@ def get_dist(bit_str):
     return int(pix_dist)
 
 def get_color(bits):
+    """
+    Objective: Get object's corresponding color
+    Returns: RGB color tuple
+    Parameters:
+        bits: 12-bit lengthed trace
+    """
     obj = get_object(bits)
     if obj == "01":
         return car_color
@@ -126,24 +144,49 @@ def get_color(bits):
         return LIGHT_GREY # background, return road's color
 
 def draw_obj(screen, color, x, y, w, h):
+    """
+    Objective: Draw a rectangle on the screen
+    Parameters:
+        screen: variable containing the visualizer's screen
+        color: RGB color tuple
+        x: rectangle's starting x position
+        y: rectangle's starting y position
+        w: width of rectangle
+        h: height of rectangle
+    """
     pygame.draw.rect(screen, color, pygame.Rect(x, y, w, h))
 
 def blit_obj(screen, obj, x, y):
-    # Draw object images
-    if obj == '01':
+    """
+    Objective: Draw different objects (car, motorcycle, truck)
+    Parameters:
+        screen: variable containing the visualizer's screen
+        obj: String of 2 bits representing the object in the lane
+        x: object's starting x position
+        y: object's starting y position
+    """
+    if obj == '01': # car
         screen.blit(get_img('images/blue-car.png'), (x, y))
-    if obj == '10':
+    if obj == '10': # motorcycle
         screen.blit(get_img('images/motorcycle.png'), (x, y))
-    if obj == '11':
+    if obj == '11': # truck
         screen.blit(get_img('images/truck.png'), (x, y))
 
+
+# MAIN FUNCTION
 def main():
-    done = False
+    """
+    Objective: Illustrate the Visualizer
+    """
+
+    # Set variables
+    done = False # while loop condition
 
     global x_left, y_left, x_mid, y_mid, x_right, y_right
     global obj_l, obj_m, obj_r
     global MOVE_DOWN
 
+    # Establish clock
     clock = pygame.time.Clock()
 
     # Create events
@@ -153,10 +196,12 @@ def main():
     pygame.time.set_timer(move_down_event, MOVE_DOWN)
 
     # Get traces
-    # Trace format: 011010111100
+    # Trace format: 3 columns per epoch with the form XY
+    #               where X is a 2-bit string representing object type ('01' car, '10' motorcycle, '11' truck) and
+    #               where Y is a 10-bit string representing distance from car (0 to 1023 in binary)
     tr = 'traces/trace_ex.txt'
     left, mid, right = parse_trace(tr)   
-    left.reverse()
+    left.reverse() # reverse list order so popping gives chronological order
     mid.reverse()
     right.reverse()
 
@@ -180,12 +225,12 @@ def main():
                 mid_bits = mid.pop()
                 right_bits = right.pop()
 
-                # parse trace bits into tuple: (pixel distance from tip of car, bits for object type)
+                # Parse trace bits into tuple: (pixel distance from tip of car, bits for object type)
                 left_tup = (get_dist(left_bits), get_object(left_bits))
                 mid_tup = (get_dist(mid_bits), get_object(mid_bits))
                 right_tup = (get_dist(right_bits), get_object(right_bits))
 
-                # update y positions and object types
+                # Update y positions and object types
                 y_left = 500 - left_tup[0] # (500 - distance) = y position
                 y_mid = 500 - mid_tup[0]
                 y_right = 500 - right_tup[0]
@@ -206,13 +251,13 @@ def main():
         if bgY2 > 500:
             bgY2 = -500
 
-        # Draw objects, need to set the obj first
+        # Draw objects every epoch
         screen.blit(get_img('images/red-car.png'), (x_mid, 440)) # your car
         blit_obj(screen, obj_l, x_left, y_left-55) # left lane
         blit_obj(screen, obj_m, x_mid, y_mid-55) # middle lane
         blit_obj(screen, obj_r, x_right, y_right-55) # right lane
 
-        # Update screen
+        # Update screen to display changes
         pygame.display.flip()
         clock.tick(60)
 
@@ -220,14 +265,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-    # left_traces, mid_traces, right_traces = parse_trace('traces/trace_ex.txt')
-    # print('left traces: ')
-    # for l in left_traces:
-    #     print(l)
-    # print('mid traces: ')
-    # for m in mid_traces:
-    #     print(m)
-    # print('right traces: ')
-    # for r in right_traces:
-    #     print(r)
     
