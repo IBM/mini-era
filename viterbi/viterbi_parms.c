7403123311a37e89df4a12562bf5630609b30424
J-D Wellman <jdwellman@yahoo.com>
2020-05-08 12:59:08 -0400

    Refactored the code somewhat; cleaner, generally smaller, etc.

---
+++ viterbi/viterbi_parms.c
@@ -0,0 +1,13 @@
+#include "base.h"
+
+ofdm_param ofdm = {   0,   //  encoding   : 0 = BPSK_1_2
+		     13,   //  rate_field : rate field ofSIGNAL header
+		      1,   //  n_bpsc     : coded bits per subcarrier
+		     48,   //  n_cbps     : coded bits per OFDM symbol
+		     24 }; //  n_dbps     : data bits per OFDM symbol
+
+frame_param frame = {  1528,    // psdu_size      : PSDU size in bytes 
+		        511,    // n_sym          : number of OFDM symbols
+		         18,    // n_pad          : number of padding bits in DATA field
+		      24528,    // n_encoded_bits : number of encoded bits
+		      12264 };  // n_data_bits    : number of data bits, including service and padding
