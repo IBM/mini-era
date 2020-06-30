269e795901c589981cd550c299c4bbcd6a03126d
Yifan Zhao <yifanz16@illinois.edu>
2020-06-29 21:49:17 -0500

    Changes the name of nvdla binary being called

--- src/main.c
+++ src/main.c
@@ -489,7 +489,7 @@ void execute_cv_kernel(/* 0 */ label_t* in_tr_val, size_t in_tr_val_size, /* 1 *
   __hpvm__hint(DEVICE);
   __hpvm__attributes(2, in_tr_val, out_label, 1, out_label);
 
-  system("./nvdla_runtime --loadable hpvm-miniera_int8.nvdla --image 2004_2.jpg --rawdump");        
+  system("./nvdla_runtime --loadable hpvm-mod.nvdla --image 2004_2.jpg --rawdump");        
   *out_label = parse_output_dimg();
   printf("---> *out_label = %d\n", *out_label);
   __hpvm__return(1, out_label_size);
