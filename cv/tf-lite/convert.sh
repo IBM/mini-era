3c1d8f4edfdf87d2692a7d4e563bbc8f677147ab
J-D Wellman <jdwellman@yahoo.com>
2021-03-09 15:03:59 -0500

    Creating an HPVM-Mini-ERA version (branch) for our Docker Release

--- cv/tf-lite/convert.sh
+++
@@ -1,1 +0,0 @@
-tflite_convert --keras_model_file  ./nhwc_model/model.h5 --output_file ./tflite_model
--- cv/tflite/third_party/tf_download_dependencies.sh
+++
@@ -1,4 +0,0 @@
-# Copyright (c) 2011-2020 Columbia University, System Level Design Group
-# SPDX-License-Identifier: Apache-2.0
-
-./tensorflow/tensorflow/lite/tools/make/download_dependencies.sh
