a61fa418c23129a1106b0934bb0c6569c88f58b3
J-D Wellman <jdwellman@yahoo.com>
2021-07-15 10:40:53 -0400

    Update to make clear these need to be set byzeroing them out

--- setup_paths.sh
+++ setup_paths.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ####### THESE VARIABLES NEED TO BE SET! #########
-#export HPVM_DIR=$HOME/work_dir/hpvm-dssoc/hpvm
-#export RISCV_BIN_DIR=$HOME/work_dir/riscv/bin
-export RISCV_BIN_DIR=/opt/riscv/bin
-export ESP_ROOT=/home/wellman/HPVM/epochs/esp
+#export RISCV_BIN_DIR=/opt/riscv/bin
+#export ESP_ROOT=/home/user/esp
+export RISCV_BIN_DIR=
+export ESP_ROOT=
 
 
 ####### THESE VARIABLES SHOULD NOT NEED ANY MODIFICATION! #########
